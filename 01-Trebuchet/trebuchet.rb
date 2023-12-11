71ad6d95131941a05ac387b8060bc985f946d84c
Alessia Moison <adm2217@columbia.edu>
2023-12-11 18:24:16 +0100

    solution of part 1 of Day 1 - trebuchet puzzle

--- 01-Trebuchet/trebuchet.rb
+++ 01-Trebuchet/trebuchet.rb
@@ -2,17 +2,25 @@
 # Read lines from the file
 input_lines = File.readlines('input.txt').map(&:chomp)
 
+processed_lines = []
+
+def to_digits(string)
+  # Remove all non-digits and ensure non-empty
+  digits = string.gsub(/\D/,'')
+  return 0 if digits.empty?
+
+  # Select the first and last digit and convert to integer
+  pair = digits[0] + digits[-1]
+  return pair.to_i
+end
 # Process each line as needed
 input_lines.each do |line|
-  # Your code to process each line
+  processed_lines << to_digits(line)
 end
 
-# Rest of your solution
+total_sum = processed_lines.sum
 
-
-# OUTPUT
-output = "Your solution output here"
+output = "Sum of all calibration values: #{total_sum}"
 
 # Write output to file
 File.write('output.txt', output)
-g
