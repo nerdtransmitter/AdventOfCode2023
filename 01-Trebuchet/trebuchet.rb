f41a8a90c1cba2f723a1153edc259d9644b83391
Alessia Moison <adm2217@columbia.edu>
2023-12-11 17:54:49 +0100

    setup for day 1

---
+++ 01-Trebuchet/trebuchet.rb
@@ -0,0 +1,18 @@
+# day 1
+# Read lines from the file
+input_lines = File.readlines('input.txt').map(&:chomp)
+
+# Process each line as needed
+input_lines.each do |line|
+  # Your code to process each line
+end
+
+# Rest of your solution
+
+
+# OUTPUT
+output = "Your solution output here"
+
+# Write output to file
+File.write('output.txt', output)
+g
